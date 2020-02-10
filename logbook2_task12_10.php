b87de86601253092a25bf1a79de0a768e95abd25
lucys11 <55978029+lucys11@users.noreply.github.com>
2020-02-10 15:05:58 +0000

    logbook 2 task 12 + 10

---
+++ logbook2_task12_10.php
@@ -0,0 +1,23 @@
+<?
+  $mymarks["year 1"] = 55;
+  $mymarks["year 2"] = 65;
+  $mymarks["year 3"] = 75;
+
+
+
+  <head>
+  <title>Data in table</title>
+  </head>
+  <body>
+  <table border=1 align="center">
+  <tr><th>Index</th><th>Subject</th></tr>
+  <?
+  while(list($index,$value) = each($mymarks))
+  {
+    echo "for  $index  my grade was  $value <br/>";
+  }
+  echo "<br/> My best year was Year 3 when I averaged ". $mymarks["year 3"];
+?>
+  </table>
+  </body>
+  </html>
