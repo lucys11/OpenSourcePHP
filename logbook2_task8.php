1b62b6c203c188d17a5925dadc25fd0d58a91a8e
lucys11 <55978029+lucys11@users.noreply.github.com>
2020-02-10 14:48:09 +0000

    logbook 2 task 8

---
+++ logbook2_task8.php
@@ -0,0 +1,14 @@
+<?
+  $topModules[0] = "Internet Systems Development";
+  $topModules[1] = "Programming 1";
+  $topModules[2] = "Programming 2”;
+  $topModules[3] = "OOAD";
+  $topModules[4] = "Software Engineering";
+  $topModules[5] = "Database Design";
+  $topModules[6] = "Web Services";
+
+  for($count == 0,$count < 5;$count++)
+  {
+    echo "$count module is $topModules[$count] <br/>";
+  }
+?>
