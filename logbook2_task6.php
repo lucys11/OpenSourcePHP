c3c06d30b41de132387ed00b3d2c9aba11eff1c5
lucys11 <55978029+lucys11@users.noreply.github.com>
2020-02-10 14:38:16 +0000

    logbook 2 task 6

--- logbook2_task6.php
+++ logbook2_task6.php
@@ -6,7 +6,7 @@
 
   echo "Index 0 = $marks[0] <br/>";
   echo "Index 1 = $marks[1] <br/>";
-  echo "Index 2 = $marks[2] <br/>”;
+  echo "Index 2 = $marks[2] <br/>";
   echo "Index 3 = $marks[3] <br/>";
   echo "Index 4 = $marks[4] <br/>";
   echo "Index 5 = $marks[5] <br/>";
