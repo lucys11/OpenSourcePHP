e9ec2ccfff829dfef7db25c42f66912243f5e029
lucys11 <55978029+lucys11@users.noreply.github.com>
2020-02-10 15:01:21 +0000

    logbook 2 task 11

--- logbook2_task11.php
+++ logbook2_task11.php
@@ -5,13 +5,14 @@
   $mymarks["module4"] = 90;
   $mymarks["module5"] = 65;
   $mymarks["module6"] = 80;
+  $total = 0;
 
   while(list($index,$value) = each($mymarks))
   {
     echo "for  $index  my marks were  $value <br/>";
+    $total = $total + $mymarks[$index];
   }
-  $total = 0;
-  $total = $total + $mymarks[$index];
+  
   $average = $total /  6;
   echo "my average is $average";
 
