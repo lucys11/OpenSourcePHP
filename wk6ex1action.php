89e3040c76aa157430e120a64666a4783c2cb4fd
lucys11 <55978029+lucys11@users.noreply.github.com>
2020-03-09 14:34:44 +0000

    logbook 6 task 1

---
+++ wk6ex1action.php
@@ -0,0 +1,17 @@
+<?php
+	$sql = "INSERT INTO test (name,email,phone_number) ";
+	$sql = $sql . " values ('$_POST[txtName]','$_POST[txtEmail]','$_POST[txtPhoneNumber]’)";
+
+	$link = mysqli_connect("localhost", "root" , "" , "db1_lucysmart");
+
+	// Execute sql statement
+
+	$sql = "SELECT * from test";
+
+	$result = mysqli_query($link, $sql);
+
+	while ($row = mysqli_fetch_assoc($result))
+	{
+		echo "$row[name]  $row[email]  $row[phone_number] <br/>";
+	}
+?>
